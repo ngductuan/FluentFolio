d000d2e8ac8d4c3a987b785805abec06a1233bdc
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-01-13 23:03:29 +0700

    update-flutter-lints

--- lib/src/features/accounts/presentation/views/sign_in_page.dart
+++ lib/src/features/accounts/presentation/views/sign_in_page.dart
@@ -4,14 +4,14 @@ class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
 
   @override
-  _SignInPageState createState() => _SignInPageState();
+  State<SignInPage> createState() => _SignInPageState();
 }
 
 class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Center(child: Text('test')),
     );
   }
 }
