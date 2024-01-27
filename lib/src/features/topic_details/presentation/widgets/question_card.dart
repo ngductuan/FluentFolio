c30acd3d29844f4dc74417b87da3721e46bf09e6
21127197 <ndtuan21@clc.fitus.edu.vn>
2024-01-27 23:51:04 +0700

    fix-dividerColor-in-question_card.dart

--- lib/src/features/topic_details/presentation/widgets/question_card.dart
+++ lib/src/features/topic_details/presentation/widgets/question_card.dart
@@ -7,75 +7,79 @@ class QuestionCard extends StatelessWidget {
   final int index;
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          color: colorsByTheme(context).backgroundCardsChip,
-          borderRadius: BorderRadius.circular(radius12),
-          boxShadow: [
-            BoxShadow(
-              color: AppColor.shadow.shade200,
-              blurRadius: 4.0,
-              spreadRadius: 0,
-              offset: const Offset(0, 2),
+  Widget build(BuildContext context) => Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: Container(
+          decoration: BoxDecoration(
+            color: colorsByTheme(context).backgroundCardsChip,
+            borderRadius: BorderRadius.circular(radius12),
+            boxShadow: [
+              BoxShadow(
+                color: AppColor.shadow.shade200,
+                blurRadius: 4.0,
+                spreadRadius: 0,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: ExpansionTile(
+            collapsedBackgroundColor:
+                colorsByTheme(context).backgroundCardsChip,
+            backgroundColor: colorsByTheme(context).backgroundCardsChip,
+            iconColor: colorsByTheme(context).expansionIcon,
+            collapsedIconColor: colorsByTheme(context).expansionIcon,
+            expandedCrossAxisAlignment: CrossAxisAlignment.start,
+            childrenPadding:
+                const EdgeInsets.fromLTRB(padding16, 0, padding8, padding16),
+            tilePadding:
+                const EdgeInsets.fromLTRB(padding16, padding8, padding8, 0),
+            title: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${index + 1}.',
+                  style: AppTextStyle(context).getDefaultStyle().bold,
+                ),
+                const SizedBox(width: spacing8),
+                Expanded(
+                  child: Text(
+                    TempNote.topicDetailQues,
+                    style: AppTextStyle(context).getDefaultStyle().bold,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        clipBehavior: Clip.hardEdge,
-        child: ExpansionTile(
-          collapsedBackgroundColor: colorsByTheme(context).backgroundCardsChip,
-          backgroundColor: colorsByTheme(context).backgroundCardsChip,
-          iconColor: colorsByTheme(context).expansionIcon,
-          collapsedIconColor: colorsByTheme(context).expansionIcon,
-          expandedCrossAxisAlignment: CrossAxisAlignment.start,
-          childrenPadding:
-              const EdgeInsets.fromLTRB(padding16, 0, padding8, padding16),
-          tilePadding:
-              const EdgeInsets.fromLTRB(padding16, padding8, padding8, 0),
-          title: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                '${index + 1}.',
-                style: AppTextStyle(context).getDefaultStyle().bold,
+                TextDoc.txtTopicAnswers,
+                style: AppTextStyle(context).getDefaultStyle().fontCaption,
               ),
-              const SizedBox(width: spacing8),
-              Expanded(
-                child: Text(
-                  TempNote.topicDetailQues,
-                  style: AppTextStyle(context).getDefaultStyle().bold,
+              const SizedBox(height: spacing2),
+              ...TempNote.topicDetailAnswer.map(
+                (e) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '•',
+                      style: AppTextStyle(context)
+                          .getDefaultStyle()
+                          .bold
+                          .setFontFamily(noToSansFont)
+                          .setTextSize(20),
+                    ),
+                    const SizedBox(width: spacing8),
+                    Expanded(
+                      child: Text(
+                        e ?? 'null answer',
+                        style: AppTextStyle(context).getDefaultStyle(),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
-          children: [
-            Text(
-              TextDoc.txtTopicAnswers,
-              style: AppTextStyle(context).getDefaultStyle().fontCaption,
-            ),
-            const SizedBox(height: spacing2),
-            ...TempNote.topicDetailAnswer.map(
-              (e) => Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '•',
-                    style: AppTextStyle(context)
-                        .getDefaultStyle()
-                        .bold
-                        .setFontFamily(noToSansFont)
-                        .setTextSize(20),
-                  ),
-                  const SizedBox(width: spacing8),
-                  Expanded(
-                    child: Text(
-                      e ?? 'null answer',
-                      style: AppTextStyle(context).getDefaultStyle(),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
         ),
       );
 }
